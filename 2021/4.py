ab5ca1c030f71f14a7580b6c63eb4f46b9cbb9a3
sasek <sasek333@gmail.com>
2022-11-25 15:59:42 +0100

    2021/3 complete

---
+++ 2021/4.py
@@ -0,0 +1,16 @@
+#! /bin/env python3
+
+def part1 (puzzle):
+    pass
+
+
+def part2 (puzzle):
+    pass
+
+puzzle = []
+
+if __name__ == "__main__":
+
+    print(part1(puzzle))
+
+    # print(part2(puzzle))
