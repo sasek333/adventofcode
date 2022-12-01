062adc3d9fd836ce12264daab7d39c4bdbf1886c
sasek <sasek333@gmail.com>
2022-12-01 09:30:02 +0100

    2021/04 added files

--- 2021/4.py
+++ 2021/4.py
@@ -1,16 +1,19 @@
 #! /bin/env python3
 
 def part1 (puzzle):
-    pass
+    return puzzle
 
 
 def part2 (puzzle):
     pass
 
-puzzle = []
 
 if __name__ == "__main__":
-
+    with open("2021/4.txt") as f:
+        puzzle = []
+        for line in f:
+            puzzle.append(line.replace('\n', ''))
+    
     print(part1(puzzle))
 
-    # print(part2(puzzle))
+    print(part2(puzzle))
