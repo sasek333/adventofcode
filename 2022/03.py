ab7b95bd8f08edc601667efb2ef904f665b5c131
sasek <sasek333@gmail.com>
2022-12-03 10:29:45 +0100

    2022/03 finished

--- 2022/03.py
+++ 2022/03.py
@@ -17,7 +17,22 @@ def part1 (puzzle):
 
 
 def part2 (puzzle):
-    pass
+    from itertools import islice, tee
+    result = 0
+    for i in range(int(len(puzzle)/3)):
+        try:
+            common = set(puzzle[3*i]).intersection(set(puzzle[3*i+1]), set(puzzle[3*i+2])).pop()
+        except KeyError:
+            continue
+
+        if common.isupper():
+           result += 26
+           common = common.lower()
+
+        result += ord(common) - ord('a') + 1        
+
+
+    return result
 
 
 if __name__ == "__main__":
@@ -26,6 +41,6 @@ if __name__ == "__main__":
         for line in f:
             puzzle.append(line.replace('\n', ''))
     
-    print(part1(puzzle))
+    # print(part1(puzzle))
 
-    # print(part2(puzzle))
+    print(part2(puzzle))
