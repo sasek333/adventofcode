20a000e61836bc296e30ee33d16fed905bdef4f7
sasek <sasek333@gmail.com>
2022-12-06 11:24:38 +0100

    2022/06

--- 2022/06.py
+++ 2022/06.py
@@ -6,13 +6,18 @@ from collections import deque
 def part1 (puzzle):
     buff = deque(puzzle[0][:4], maxlen=4)
     for index, elem in enumerate(puzzle[0][4:]):
+        # print(f"{buff=} {index=} {elem=}")
         buff.append(elem)
         if ('0' not in buff) and (len(set(buff)) == 4):        
             return index+5
 
-
 def part2 (puzzle):
-    pass
+    buff = deque(puzzle[0][:14], maxlen=14)
+    for index, elem in enumerate(puzzle[0][14:]):
+        print(f"{buff=} {index=} {elem=}")
+        buff.append(elem)
+        if ('0' not in buff) and (len(set(buff)) == 14):        
+            return index+15
 
 
 if __name__ == "__main__":
@@ -21,6 +26,6 @@ if __name__ == "__main__":
         for line in f:
             puzzle.append(line.replace('\n', ''))
     
-    print(part1(puzzle))
+    # print(part1(puzzle))
 
-    # print(part2(puzzle))
+    print(part2(puzzle))
