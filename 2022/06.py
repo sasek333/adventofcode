6fc987afc164f5b47329f4d43198e6924429910c
sasek <sasek333@gmail.com>
2022-12-06 11:20:41 +0100

    mend

--- 2022/06.py
+++ 2022/06.py
@@ -5,9 +5,7 @@ from collections import deque
 
 def part1 (puzzle):
     buff = deque(puzzle[0][:4], maxlen=4)
-    print(f"{buff}")
     for index, elem in enumerate(puzzle[0][4:]):
-        print(f"{index=} {buff=} {elem=}")
         buff.append(elem)
         if ('0' not in buff) and (len(set(buff)) == 4):        
             return index+5
